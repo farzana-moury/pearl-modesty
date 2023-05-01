2283607fa4a201e6acf720fc16f5edae8a4ed296
farzana-moury <w0737626@myscc.ca>
2023-05-01 19:23:57 -0400

    new: display shop items

--- src/App.js
+++ src/App.js
@@ -16,12 +16,19 @@ import looseWear from "./images/loose-wear.png";
 import accessories from "./images/accessories.png";
 
 const DATA = [
-  { id: `collections-${nanoid()}`, name: "Abayas", image: abayas},
+  { id: `collections-${nanoid()}`, name: "Abayas", image: abayas },
   { id: `collections-${nanoid()}`, name: "Hijabs", image: hijabs },
   { id: `collections-${nanoid()}`, name: "Niqabs", image: niqabs },
   { id: `collections-${nanoid()}`, name: "Dresses", image: dresses },
   { id: `collections-${nanoid()}`, name: "Loose Wear", image: looseWear },
   { id: `collections-${nanoid()}`, name: "Accessories", image: accessories }
+];
+
+const NEW_ARRIVALS = [
+  { id: `collections-${nanoid()}`, name: "Luxury Abaya", image: abayas, price: 50.00.toFixed(2) },
+  { id: `collections-${nanoid()}`, name: "Black Chiffon Hijab", image: hijabs, price: 12.00.toFixed(2) },
+  { id: `collections-${nanoid()}`, name: "Satin Creme Dress", image: niqabs, price: 75.00.toFixed(2) },
+  { id: `collections-${nanoid()}`, name: "Dessert Jilbab", image: dresses, price: 30.00.toFixed(2) }
 ];
 
 function App() {
@@ -33,8 +40,8 @@ function App() {
 render(
   <Router>
     <Home path='/' />
-    <New path='new' />
-    <Shop path='shop' collections={DATA}/>
+    <New path='new' arrivals={NEW_ARRIVALS} />
+    <Shop path='shop' collections={DATA} />
     <AboutUs path='about-us' />
   </Router>
 )
--- src/components/new/New.js
+++ src/components/new/New.js
@@ -1,10 +1,30 @@
 import React from "react";
 import './new.css';
 
-export default function New() {
+export default function New(props) {
+    const newArrivalsList = props.arrivals.map((arrival) => 
+        <Item
+            key={arrival.id}
+            id={arrival.id}
+            name={arrival.name}
+            image={arrival.image}
+            price={arrival.price}
+        />
+    )
     return (
         <div className="new">
-            <h1>New Arrivals</h1>
+            <h1><strong>NEW ARRIVALS</strong></h1>
+            {newArrivalsList}
+        </div>
+    )
+}
+
+const Item = (props) => {
+    return(
+        <div className="shop-item">
+            <img src={props.image} alt="shopping item" className="image"/>
+            <p>{props.name}</p>
+            <p id="price">${props.price} CAD</p>
         </div>
     )
 }
