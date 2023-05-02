7e81f410e68f5fcb66a38fbeda0451fbc49d3973
farzana-moury <w0737626@myscc.ca>
2023-05-02 19:00:42 -0400

    new: display individual shop item when clicked

--- src/components/new/New.js
+++ src/components/new/New.js
@@ -1,8 +1,36 @@
 import React from "react";
 import './new.css';
+import { useState } from "react";
 
 export default function New(props) {
-    const newArrivalsList = props.arrivals.map((arrival) => 
+    const [isViewing, setViewing] = useState(false);
+    const [currentItem, setCurrentItem] = useState();
+
+    const Item = (props) => {
+        return (
+            <div className="shop-item">
+                <img src={props.image} alt="shopping item" className="image" onClick={() => shoppingItemClicked(props) }  />
+                <p>{props.name}</p>
+                <p id="price">${props.price} CAD</p>
+            </div>
+        )
+    }
+
+    const ViewItem = (props) => {
+        return(
+            <div onClick={() => setViewing(false)}>
+                {props.item.name}
+                <img src={props.item.image}></img>
+            </div>
+        )
+    }
+
+    function shoppingItemClicked(item){
+        setViewing(true);
+        setCurrentItem(item);
+    }
+
+    const newArrivalsList = props.arrivals.map((arrival) =>
         <Item
             key={arrival.id}
             id={arrival.id}
@@ -11,20 +39,15 @@ export default function New(props) {
             price={arrival.price}
         />
     )
+
     return (
+        isViewing ?
+        <ViewItem item={currentItem}/>
+        :
         <div className="new">
             <h1><strong>NEW ARRIVALS</strong></h1>
             {newArrivalsList}
         </div>
     )
-}
 
-const Item = (props) => {
-    return(
-        <div className="shop-item">
-            <img src={props.image} alt="shopping item" className="image"/>
-            <p>{props.name}</p>
-            <p id="price">${props.price} CAD</p>
-        </div>
-    )
 }
---
+++ src/components/view item/ViewItem.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default function ViewItem(props) {
+    
+    return(
+        <div>{props.item.name}</div>
+    )
+}
